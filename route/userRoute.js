1b525779bc78628f5453d8a55e36aa96fb3f7d39
rakeshsm119 <rakeshsm119@gmail.com>
2022-11-23 11:01:20 +0530

    first  commit of cms-v1.0

---
+++ route/userRoute.js
@@ -0,0 +1,13 @@
+const route = require('express').Router()
+const userController = require('../controller/userController')
+const { use } = require('./authRoute')
+
+route.get('/allUser', userController.getAll)
+route.get('/currentUser', userController.getCurrentUser)
+
+route.patch('/update/:id', userController.updateUser)
+route.delete('/delete/:id', userController.deleteUser)
+
+route.patch('/changeRole/:id', userController.changeRole)
+
+module.exports = route
